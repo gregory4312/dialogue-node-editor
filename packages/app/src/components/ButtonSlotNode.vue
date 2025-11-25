adb25ef0ee3a504dd1b598d952060e52776b9493
Aevarkan <aevarkan@gmail.com>
2025-11-25 18:43:51 +0800

    fix(app): button node emits events properly

--- packages/app/src/components/ButtonSlotNode.vue
+++ packages/app/src/components/ButtonSlotNode.vue
@@ -2,22 +2,32 @@
 import type { VisualSlot } from '@/types';
 import { type NodeProps } from '@vue-flow/core'
 import type { Button } from '@workspace/common';
-import { computed, ref } from 'vue';
+import { ref, watch } from 'vue';
 
 const props = defineProps<NodeProps<VisualSlot>>()
 
-const buttonRef = ref<Button | null>(props.data.button ?? null)
 
-const buttonComputed = computed({
-  get: () => buttonRef?.value,
-  set: (button: Button | null) => {
-    buttonRef.value = button
-    emit('editButton', props.data.parentSceneId, props.data.id, props.data.index, button)
-  }
-})
+// const buttonRef = computed({
+//   get: () => props.data.button,
+//   set: (button: Button) => {
+//     emit('editButton', props.data.parentSceneId, props.data.id, props.data.index, { ...button })
+//     console.log({...button})
+//   }
+// })
 
+const buttonRef = ref<Button>({ ...props.data.button })
+
+watch(
+    buttonRef,
+    (newButton) => {
+      emit('editButton', props.data.parentSceneId, props.data.id, props.data.index, { ...newButton })
+      // console.log({...newButton})
+    },
+    { deep: true }
+  )
+  
 const emit = defineEmits<{
-  (event: 'editButton', parentSceneId: string, nodeId: string, buttonIndex: number, button: Button | null): void
+  (event: 'editButton', parentSceneId: string, nodeId: string, buttonIndex: number, button: Button): void
 }>()
 </script>
 
@@ -30,9 +40,9 @@ const emit = defineEmits<{
       {{ props.data.index + 1 }}
     </p>
     <!-- show button info if it exists -->
-    <div v-if="buttonComputed">
-      <input v-model="buttonComputed.displayName" />
-      <input v-model="buttonComputed.commands" />
+    <div v-if="buttonRef">
+      <input v-model="buttonRef.displayName" />
+      <input v-model="buttonRef.commands" />
     </div>
   </div>
 </template>
